80cbf4c133b2517118be4e020f43a0664da1aef7
Charles Jacob <jacobcharles56@gmail.com>
2012-10-17 11:25:55 +0200

    cuir cuir moustache 1st commit

---
+++ fuel/app/logs/2012/10/17.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-10-17 08:41:39 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
